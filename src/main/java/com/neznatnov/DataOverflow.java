1c291ee7df68db059ea57aa130b3e9bde37ee8c4
Veronika Iatckaia <neznatnov@gmail.com>
2023-04-10 16:27:19 +0200

    letter case change

--- src/main/java/com/neznatnov/DataOverflow.java
+++ src/main/java/com/neznatnov/DataOverflow.java
@@ -4,30 +4,30 @@ public class DataOverflow {
     public static void main(String[] args) {
 
         // хранит целое число от -128 до 127 и занимает 1 байт
-        byte FirstByte = 100;
-        byte SecondByte = 100;
-        byte SumByte = (byte) (FirstByte + SecondByte);
-        System.out.println(SumByte);
+        byte firstByte = 100;
+        byte secondByte = 100;
+        byte sumByte = (byte) (firstByte + secondByte);
+        System.out.println(sumByte);
 
         // хранит целое число от -32768 до 32767 и занимает 2 байта
-        short FirstShort = 30000;
-        short SecondShort = 10000;
-        short SumShort = (short) (FirstShort + SecondShort);
-        System.out.println(SumShort);
+        short firstShort = 30000;
+        short secondShort = 10000;
+        short sumShort = (short) (firstShort + secondShort);
+        System.out.println(sumShort);
 
 
         // хранит целое число от -2147483648 до 2147483647 и занимает 4 байта
-        int FirstInt = 2000000000;
-        int SecondInt = 2000000000;
-        int SumInt = (FirstInt + SecondInt);
-        System.out.println(SumInt);
+        int firstInt = 2000000000;
+        int secondInt = 2000000000;
+        int sumInt = (firstInt + secondInt);
+        System.out.println(sumInt);
 
 
         // хранит целое число от –9 223 372 036 854 775 808 до 9 223 372 036 854 775 807 и занимает 8 байт
-        long FirstLong = 9000000000000000000L;
-        long SecondLong = 9000000000000000000L;
-        long SumLong = (FirstLong + SecondLong);
-        System.out.println(SumLong);
+        long firstLong = 9000000000000000000L;
+        long secondLong = 9000000000000000000L;
+        long sumLong = (firstLong + secondLong);
+        System.out.println(sumLong);
 
 
     }
